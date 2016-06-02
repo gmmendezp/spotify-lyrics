13cf20b5ab1e42469675bec090807d0d54794501
Martin Mendez <gmmendezp@gmail.com>
2016-06-01 22:43:25 -0600

    Added check for OS.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 "use strict";
 var lyrics = require('./lyrics'),
-    spotify = require('@jonny/spotify-web-helper')(),
-    dbus = require('dbus'),
     app = require('express')(),
     server = require('http').Server(app),
     io = require('socket.io')(server);
+
+var dbus;
+var spotify
+if(/^win/.test(process.platform) || /^darwin/.test(process.platform)) {
+  spotify = require('@jonny/spotify-web-helper')();
+} else {
+   dbus = require('dbus');
+}
 
 var current = {};
 var last = {
@@ -20,6 +26,7 @@ var last = {
     }
   }
 };
+
 var areEqualTracks = (track1, track2) => (track1.track.artist_resource.name === track2.track.artist_resource.name &&
   track1.track.track_resource.name === track2.track.track_resource.name &&
   track1.track.album_resource.name === track2.track.album_resource.name);
@@ -51,35 +58,37 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-spotify.player.on('ready', function() {
-  console.log('Express server listening to port 8000.');
-  updateCurrent(spotify.status);
-  spotify.player.on('play', function() {
+
+if(/^win/.test(process.platform) || /^darwin/.test(process.platform)) {
+  spotify.player.on('ready', function() {
+    console.log('Express server listening to port 8000.');
     updateCurrent(spotify.status);
-  });
-  spotify.player.on('track-change', function(track) {
-    updateCurrent({track});
-  });
-});
-
-var bus = new dbus();
-var sessionBus = bus.getBus('session')
-
-sessionBus.getInterface('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2', 'org.freedesktop.DBus.Properties', function(err, iface){
-  if(err) return console.log(err);
-  iface.on('PropertiesChanged', function(name, data) {
-		updateCurrent({
-      track: {
-        artist_resource: {
-          name: data.Metadata['xesam:artist'][0]
-        },
-        track_resource: {
-          name: data.Metadata['xesam:title']
-        },
-        album_resource: {
-          name: data.Metadata['xesam:album']
-        }
-      }
+    spotify.player.on('play', function() {
+      updateCurrent(spotify.status);
     });
-	});
-});
+    spotify.player.on('track-change', function(track) {
+      updateCurrent({track});
+    });
+  });
+} else {
+  var bus = new dbus();
+  var sessionBus = bus.getBus('session')
+  sessionBus.getInterface('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2', 'org.freedesktop.DBus.Properties', function(err, iface){
+    if(err) return console.log(err);
+    iface.on('PropertiesChanged', function(name, data) {
+      updateCurrent({
+        track: {
+          artist_resource: {
+            name: data.Metadata['xesam:artist'][0]
+          },
+          track_resource: {
+            name: data.Metadata['xesam:title']
+          },
+          album_resource: {
+            name: data.Metadata['xesam:album']
+          }
+        }
+      });
+    });
+  });
+}
